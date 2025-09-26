1c0f54211cc4c7fff4a371e0046c4ce35605741b
cesarleyton <cesarleyton549@gmail.com>
2025-09-26 11:21:51 -0500

    add tree object reading

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -8,8 +8,16 @@ import (
 	"io"
 	"os"
 	"slices"
+	"sort"
 	"strconv"
+	"strings"
 )
+
+type TreeEntry struct {
+	shal     []byte
+	filemode string
+	filename string
+}
 
 func main() {
 
@@ -18,6 +26,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	// fileModes := map[string]int{
+	// 	"normal":    100644,
+	// 	"exe":       100755,
+	// 	"symbolic":  120000,
+	// 	"directory": 400000,
+	// }
 	switch command := os.Args[1]; command {
 
 	case "init":
@@ -28,6 +42,7 @@ func main() {
 		}
 
 		headFileContents := []byte("ref: refs/heads/main\n")
+
 		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
 			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
 		}
@@ -35,16 +50,19 @@ func main() {
 		fmt.Println("Initialized git directory")
 
 	case "hash-object":
-		param := os.Args[3]
 
-		file, err := os.ReadFile(param)
+		filePath := os.Args[3]
+		// fmt.Println("ff", filePath)
+		file, err := os.ReadFile(filePath)
+
 		if err != nil {
-			fmt.Println("unable to find file")
-			return
+			fmt.Println("err reading", err)
+			os.Exit(1)
 		}
+
 		h := sha1.New()
+
 		hashInput := "blob " + strconv.Itoa(len(file)) + "\x00" + string(file)
-		// fmt.Println("hash input", hashInput)
 
 		h.Write([]byte(hashInput))
 
@@ -55,47 +73,51 @@ func main() {
 		errCreating := os.MkdirAll(".git/objects/"+hash[0:2], 0775)
 
 		if errCreating != nil {
-			fmt.Println("fuck 1")
-			return
+			fmt.Println("err c", errCreating)
+			os.Exit(1)
 		}
 
 		f, err := os.Create(path)
 
 		if err != nil {
-			fmt.Println("fuck 2")
-			return
+			fmt.Println("err c2", err)
+			os.Exit(1)
 		}
+
 		zlibW := zlib.NewWriter(f)
+
 		_, errCom := zlibW.Write([]byte(hashInput))
+
 		if errCom != nil {
-			fmt.Println("fuck 3")
-			return
+			os.Exit(1)
 		}
+
 		zlibW.Close()
+
 		fmt.Println(hash)
 
 	case "cat-file":
 
-		param := os.Args[3]
+		shal := os.Args[3]
 
-		file, err := os.ReadFile(".git/objects/" + param[0:2] + "/" + param[2:])
+		file, err := os.ReadFile(".git/objects/" + shal[0:2] + "/" + shal[2:])
 
 		if err != nil {
-			fmt.Println("Im a retard")
+			os.Exit(1)
 			return
 		}
 
 		byteReader := bytes.NewReader(file)
-
 		zlibReader, err := zlib.NewReader(byteReader)
+
+		defer zlibReader.Close()
 
 		if err != nil {
 			fmt.Printf("Something went wrong decompressing %v\n", err)
 			os.Exit(1)
 		}
 
-		//Don't forget to close the reader and avoid memory leaks
-		defer zlibReader.Close()
+		//close the reader and avoid memory leaks
 
 		//decompressed bytes
 		decompressedData, err := io.ReadAll(zlibReader)
@@ -112,8 +134,92 @@ func main() {
 
 		fmt.Printf("%s", string(content))
 
+	case "ls-tree":
+
+		// fmt.Println("args", os.Args)
+
+		flag := os.Args[2]
+		treeHash := os.Args[3]
+
+		treePath := ".git/objects/" + treeHash[0:2] + "/" + treeHash[2:]
+
+		// fmt.Println("tree", treeHash)
+
+		file, err := os.ReadFile(treePath)
+
+		if err != nil {
+			fmt.Println("ERR:", err)
+			os.Exit(1)
+		}
+
+		byteReader := bytes.NewReader(file)
+
+		zlibReader, err := zlib.NewReader(byteReader)
+
+		data, err := io.ReadAll(zlibReader)
+
+		i := slices.Index(data, '\x00')
+
+		files := ParseFiles(data[i+1:])
+		sort.Slice(files, func(i, j int) bool {
+			return files[i].filename < files[j].filename
+		})
+
+		if flag == "--name-only" {
+			for _, f := range files {
+				fmt.Println(f.filename)
+			}
+		}
+
+		defer zlibReader.Close()
+
+		if err != nil {
+			fmt.Printf("Something went wrong decompressing %v\n", err)
+			os.Exit(1)
+		}
+
 	default:
 		os.Exit(1)
 
 	}
+}
+
+func ParseFiles(files []byte) []TreeEntry {
+
+	//Here i must extract the file mode, name and hash from each file, all of them are separated
+	// with a null byte
+
+	entries := make([]TreeEntry, 0)
+
+	i := 0
+	entry := ""
+
+	for i < len(files) {
+
+		//Everything before first null byte is the filemode and filename,then
+		// after the null byte, the 20 next characters are the hash and thats it
+
+		if files[i] == '\x00' {
+			start := i + 1
+			end := start + 20
+			hash := files[start:end]
+			// fmt.Printf("%s %x \n", entry, hash)
+			splittedEntry := strings.Split(entry, " ")
+			// fmt.Printf("%q \n", splittedEntry[1])
+			tEntry := TreeEntry{
+				filemode: splittedEntry[0],
+				filename: splittedEntry[1],
+				shal:     hash,
+			}
+			entries = append(entries, tEntry)
+			i = end
+			entry = ""
+			continue
+		}
+		entry += string(files[i])
+
+		i++
+
+	}
+	return entries
 }
